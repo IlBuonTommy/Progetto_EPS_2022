6d3fcd9e57253580e2bd91400e13ed40d9f88cbd
Paolo <paolo.biolghini@gmail.com>
2022-06-02 15:54:18 +0200

    sium

--- Code/src/it/unibs/eps/Prova.java
+++ Code/src/it/unibs/eps/Prova.java
@@ -2,7 +2,7 @@ package it.unibs.eps;
 
 public class Prova {
 
-	public static void main(String args) {
-		System.out.println("ciao");
-	}
+	  public static void main(String[] args) {
+	        System.out.println("siummm");
+	    }
 }
