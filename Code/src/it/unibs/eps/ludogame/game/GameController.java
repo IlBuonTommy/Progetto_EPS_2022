84e68f0c6999ec22e928ee8a86809538f497a4bd
Alessio <a.pellicardi@gmail.com>
2022-06-03 13:47:26 +0200

    lavoro su server

---
+++ Code/src/it/unibs/eps/ludogame/game/GameController.java
@@ -0,0 +1,5 @@
+package it.unibs.eps.ludogame.game;
+
+public class GameController {
+
+}
--- Code/src/it/unibs/eps/ludogame/server/ServerController.java
+++ Code/src/it/unibs/eps/ludogame/server/ServerController.java
@@ -1,5 +1,15 @@
 package it.unibs.eps.ludogame.server;
 
-public class ServerController {
+import java.util.HashMap;
 
+import org.json.JSONObject;
+
+
+//Serve da tramite tra server e client thread, per mandare e ricevere file JSON
+
+public class ServerController {
+	private String serverName = "ludo-game";
+	private HashMap<ClientThread, PlayerColor> clients = new HashMap<ClientThread, PlayerColor>();
+	private GameController game = new Game();
+	
 }
