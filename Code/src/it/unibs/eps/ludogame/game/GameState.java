b368ceacd23bcbfce76b9134ae0b34bc70b62524
Alessio <a.pellicardi@gmail.com>
2022-06-08 15:45:50 +0200

    Ciao

--- Code/src/it/unibs/eps/ludogame/game/GameState.java
+++ Code/src/it/unibs/eps/ludogame/game/GameState.java
@@ -1,24 +1,12 @@
 package it.unibs.eps.ludogame.game;
 
-/**
-* <h1>GameState</h1>
-* <p>ENUM that contains the valid game states defined in
-* update in the MAEDN protocol.</p>
-*
-* @version 1.0
-* @since   2021-07-23
-*/
+
 public enum GameState {
 	WAITINGFORPLAYERS,
 	RUNNING,
 	FINISHED;
 	
-	/**
-	 * <h1><i>toString</i></h1>
-	 * <p>This method is converting the ENUM to strings that are supported
-	 * by the MAEDN specification.</p>
-	 * @return String - return string supported by MAEDN specification
-	 */
+	
 	@Override
 	public String toString() {
 		switch(this) {
