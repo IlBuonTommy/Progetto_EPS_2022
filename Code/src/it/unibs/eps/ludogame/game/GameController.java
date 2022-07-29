1d664dfd94d60ea8364f59f0d0e260e8eb1942c8
IlBuonTommy <tomamsobianchin23@gmail.com>
2022-07-29 12:42:03 +0200

    togliamo JSON

--- Code/src/it/unibs/eps/ludogame/game/GameController.java
+++ Code/src/it/unibs/eps/ludogame/game/GameController.java
@@ -1,13 +1,11 @@
 package it.unibs.eps.ludogame.game;
 
-import org.json.JSONObject;
-
 import it.unibs.eps.ludogame.game.*;
 
 //TODO: gestione gioco
 public interface GameController {
 	
-	//Genera un nuovo giocatore, gli assegna un colore e controlla se ci sono già 4 giocatori
+	//Genera un nuovo giocatore, gli assegna un colore e controlla se ci sono giï¿½ 4 giocatori
 	public PlayerColor register(PlayerColor requestedColor, String name, String clientName, Float clientVersion);
 	
 	//Rimuove giocatore
@@ -22,18 +20,9 @@ public interface GameController {
 	//Ritorna il giocatore corrente
 	public PlayerColor currentPlayer();
 	
-	//ritorna true se il giocatore corrente è un bot
+	//ritorna true se il giocatore corrente ï¿½ un bot
 	public Boolean currentPlayerIsBot();
-	
-	
-	//riporta lo stato del gioco in formato json
-	public JSONObject toJSON();
-	
-	//selected=-1 --> ritorna i turni possibili del giocatore
-	//Altro selected --> esegue il turno selezionato
-	public JSONObject turn(Integer selected);
-	
-	
+
 	//esegue il turno del bot nel tempo indicato
 	public void botTurn(long millis);
 	
