f2c2158b4d66085ed69e560500dc81d58f81f532
Alessio <a.pellicardi@gmail.com>
2022-08-28 18:19:00 +0200

    Gestione stringhe pt2

--- Code/src/it/unibs/eps/ludogame/testnetworking/GestioneStringhe.java
+++ Code/src/it/unibs/eps/ludogame/testnetworking/GestioneStringhe.java
@@ -18,13 +18,34 @@ public class GestioneStringhe {
 		}
 		return arrayRic;
 	}
+	
+	public static String encodeConTipo(int[] arrayDaCodificare,String tipoArray) {
+		String stringaFormattata = "";
+		for(int i=0;i<arrayDaCodificare.length;i++) {
+			stringaFormattata = stringaFormattata.concat(String.valueOf(arrayDaCodificare[i]).concat("#"));
+		}
+		return tipoArray.concat("@").concat(stringaFormattata);
+	}
+	
+	public static String[] decodeTipo(String daDecodificare) {
+		String[] stringaRicevuta = daDecodificare.split("@");
+		return stringaRicevuta;
+	}
+	
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] daMandare = {1,2,-4,3};
-		String stringaFormattata = encode(daMandare);
+		
+		String stringaFormattata = encodeConTipo(daMandare,"base");
 		System.out.println("stringaFormattata: "+ stringaFormattata);
-		int[] arrayRicevuto = decode(stringaFormattata,daMandare.length);
+		
+		String[] tipoDecodificato = decodeTipo(stringaFormattata);
+		System.out.println(Arrays.toString(tipoDecodificato));
+		System.out.println("L'array è di tipo:  "+ tipoDecodificato[0]);
+		
+		int[] arrayRicevuto = decode(tipoDecodificato[1],daMandare.length);
 		System.out.println("arrayRicevuto:   "+ Arrays.toString(arrayRicevuto));
 		
 		
