7c33b6aaea0f3fb5cd826c29fd633c51b92d1c89
IlBuonTommy <tomamsobianchin23@gmail.com>
2022-09-07 20:19:37 +0200

    el sus

--- Code/src/it/unibs/eps/ludogame/game/GameModel.java
+++ Code/src/it/unibs/eps/ludogame/game/GameModel.java
@@ -205,7 +205,7 @@ public class GameModel implements Serializable{
         //controlla se deve andare in finale
         boolean finaleT = false;
         if(plancia[posizione].getColore()==0){
-            if(posizione <= 39 && nuovaPosizione >= 0 && posizione > 33){
+            if(posizione <= 39 && posizione+valoreDado-40>=0 && posizione > 33){
                 finaleT = true;
             }
         }else{
